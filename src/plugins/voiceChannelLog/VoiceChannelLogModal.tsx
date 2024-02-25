ceb9922fee7b81756ef2453c9a5a242edb9fb452
Sqaaakoi <sqaaakoi-git@sqaaakoi.xyz>
2024-02-25 23:44:45 +1300

    VoiceChannelLog: Modal entries with timestamps, icons, and avatars. Text content not included yet

--- src/plugins/voiceChannelLog/VoiceChannelLogModal.tsx
+++ src/plugins/voiceChannelLog/VoiceChannelLogModal.tsx
@@ -6,16 +6,19 @@
 
 import { classNameFactory } from "@api/Styles";
 import { ModalCloseButton, ModalContent, ModalHeader, ModalProps, ModalRoot, ModalSize, openModal } from "@utils/modal";
-import { findByPropsLazy, findExportedComponentLazy } from "@webpack";
+import { LazyComponent } from "@utils/react";
+import { filters, find, findByPropsLazy, findExportedComponentLazy } from "@webpack";
 import { React, ScrollerThin, Text } from "@webpack/common";
 import { Channel } from "discord-types/general";
 
 import { getVcLogs, vcLogSubscribe, VoiceChannelLogEntry } from "./logs";
+import { VoiceChannelLogEntryComponent } from "./VoiceChannelLogEntryComponent";
 
 const IconClasses = findByPropsLazy("icon", "acronym", "childWrapper");
 const FriendRow = findExportedComponentLazy("FriendRow");
 
 const cl = classNameFactory("vc-voice-channel-log-");
+const { avatar, clickable } = find(filters.byProps("avatar", "zalgo", "clickable"));
 
 export function openVoiceChannelLog(channel: Channel) {
     return openModal(props => (
@@ -26,28 +29,29 @@ export function openVoiceChannelLog(channel: Channel) {
     ));
 }
 
-export function VoiceChannelLogModal({ channel, props }: { channel: Channel; props: ModalProps; }) {
-    // [].sort((a, b) => a.type - b.type);
+export const VoiceChannelLogModal = LazyComponent(() => {
 
-    React.useSyncExternalStore(vcLogSubscribe, () => getVcLogs(channel.id));
+    return function VoiceChannelLogModal({ channel, props }: { channel: Channel; props: ModalProps; }) {
+        React.useSyncExternalStore(vcLogSubscribe, () => getVcLogs(channel.id));
 
-    return (
-        <ModalRoot
-            {...props}
-            size={ModalSize.LARGE}
-        >
-            <ModalHeader>
-                <Text className={cl("header")} variant="heading-lg/semibold" style={{ flexGrow: 1 }}>{channel.name} logs</Text>
-                <ModalCloseButton onClick={props.onClose} />
-            </ModalHeader>
+        return (
+            <ModalRoot
+                {...props}
+                size={ModalSize.LARGE}
+            >
+                <ModalHeader>
+                    <Text className={cl("header")} variant="heading-lg/semibold" style={{ flexGrow: 1 }}>{channel.name} logs</Text>
+                    <ModalCloseButton onClick={props.onClose} />
+                </ModalHeader>
 
-            <ModalContent>
-                <ScrollerThin fade className={cl("scroller")}>
-                    {getVcLogs(channel.id).map((logEntry: VoiceChannelLogEntry) => {
-                        return JSON.stringify(logEntry);
-                    })}
-                </ScrollerThin>
-            </ModalContent>
-        </ModalRoot>
-    );
-}
+                <ModalContent>
+                    <ScrollerThin fade className={cl("scroller")}>
+                        {getVcLogs(channel.id).map((logEntry: VoiceChannelLogEntry) => {
+                            return <VoiceChannelLogEntryComponent logEntry={logEntry} channel={channel} />;
+                        })}
+                    </ScrollerThin>
+                </ModalContent>
+            </ModalRoot >
+        );
+    };
+});
