e3aaf3598fe7a0fd0a2821b3f7f5b281b4fd6495
Sqaaakoi <sqaaakoi-git@sqaaakoi.xyz>
2024-06-24 09:38:57 +1200

    friends only welcome message system unfinished

--- src/plugins/_core/sqaaakoiForkSupport/WelcomeModal.tsx
+++ src/plugins/_core/sqaaakoiForkSupport/WelcomeModal.tsx
@@ -6,21 +6,17 @@
 
 import { DataStore } from "@api/index";
 import { closeModal, ModalContent, ModalFooter, ModalHeader, ModalProps, ModalRoot, ModalSize, openModal } from "@utils/modal";
-import { extractAndLoadChunksLazy, findByPropsLazy, findLazy } from "@webpack";
-import { Button, Flex, RelationshipStore, Text, useEffect, useState } from "@webpack/common";
+import { extractAndLoadChunksLazy, findLazy } from "@webpack";
+import { Button, Flex, Parser, RelationshipStore, Text, useEffect, useState } from "@webpack/common";
 
-import { CURRENT_WELCOME_NOTICE_VERSION, SQAAAKOI_USER_ID, WELCOME_MESSAGE, WELCOME_NOTICE_VERSION_KEY } from "./constants";
+import { CURRENT_WELCOME_NOTICE_VERSION, SQAAAKOI_USER_ID, WELCOME_BACK_HEADER, WELCOME_HEADER, WELCOME_MESSAGE, WELCOME_NOTICE_VERSION_KEY } from "./constants";
 
-const { reactParserFor, defaultRules, parse } = findByPropsLazy("reactParserFor", "defaultRules");
 const contentClass = findLazy(m => m.modal && m.content && Object.keys(m).length === 2);
-const { getDefaultRules } = findByPropsLazy("getDefaultRules");
-const textStyles = findByPropsLazy("improved", "socialLink");
-
 // must be called before rendering
 export const requireContentClass = extractAndLoadChunksLazy(["hasAnyModalOpen)())&&(0,"]);
 
 // First time run card
-export function WelcomeModal({ modalProps, close, isFriend, force, text }: { modalProps: ModalProps; close: () => void; isFriend: boolean; force: boolean; text: string; }) {
+export function WelcomeModal({ modalProps, close, isFriend, force, welcomeBack, text }: { modalProps: ModalProps; close: () => void; isFriend: boolean; force: boolean; welcomeBack: boolean; text: string; }) {
     const [unlocked, setUnlocked] = useState(force);
     const [closing, setClosing] = useState(false);
     useEffect(() => {
@@ -35,11 +31,11 @@ export function WelcomeModal({ modalProps, close, isFriend, force, text }: { mod
 
     return <ModalRoot {...modalProps} size={ModalSize.MEDIUM} >
         <ModalHeader>
-            <Text variant="heading-lg/semibold" style={{ flexGrow: 1, textAlign: "center" }}>Welcome!</Text>
+            <Text variant="heading-lg/semibold" style={{ flexGrow: 1, textAlign: "center" }}>{welcomeBack ? WELCOME_BACK_HEADER : WELCOME_HEADER}</Text>
         </ModalHeader>
         <ModalContent>
             <div className={contentClass.content}>
-                {reactParserFor(defaultRules)(text, false)}
+                {Parser.reactParserFor(defaultRules)(text, false)}
             </div>
         </ModalContent>
         <ModalFooter>
@@ -70,6 +66,7 @@ export async function openWelcomeModal(force: boolean) {
             close={() => closeModal(key)}
             isFriend={isFriend}
             force={force}
+            welcomeBack={currentVersion === 0 || force}
             text={WELCOME_MESSAGE(isFriend)}
         />
     ), {
--- src/plugins/_core/sqaaakoiForkSupport/constants.ts
+++ src/plugins/_core/sqaaakoiForkSupport/constants.ts
@@ -13,34 +13,41 @@ export const SQAAAKOI_USER_ID = Devs.Sqaaakoi.id + "";
 export const CURRENT_WELCOME_NOTICE_VERSION = 1;
 export const WELCOME_NOTICE_VERSION_KEY = "SqaaakoiForkSupport_StartupMessageVersion";
 
+// friends or not
+const F = (strings: { raw: readonly string[] | ArrayLike<string>; }, ...args: any[]) => [true, String.raw(strings, ...args)] as [boolean, string];
+const N = (strings: { raw: readonly string[] | ArrayLike<string>; }, ...args: any[]) => [false, String.raw(strings, ...args)] as [boolean, string];
+
 // horrible
-function friendsOnlyFilter(template: readonly string[], ..._substitutions: (string | [0 | 1, string])[]): (isFriend: boolean) => string {
+function friendsOnlyFilter(_template: { raw: readonly string[]; }, ..._substitutions: (string | [boolean, string])[]): (isFriend: boolean) => string {
     const substitutions = [..._substitutions];
+    const template = _template.raw;
     return isFriend => {
         const out: string[] = [];
         for (let i = 0; i < template.length; i++) {
             out.push(template[i]);
             if (i < substitutions.length) {
                 if (Array.isArray(substitutions[i])) {
-                    if (isFriend === !!substitutions[i][0]) out.push(substitutions[i][1]);
+                    if (isFriend === substitutions[i][0]) out.push(substitutions[i][1]);
                 } else {
                     // @ts-ignore
                     out.push(substitutions[i]);
                 }
             }
         }
-        return out.join("");
+        return out.join("").trim();
     };
 }
+
+export const WELCOME_HEADER = "Welcome!";
+export const WELCOME_BACK_HEADER = "Whats New!";
 
 export const WELCOME_MESSAGE = friendsOnlyFilter`
 # Thanks for installing!
 You are running version ${gitHash}
-${[1, "hi friend!"]}
-${[0, "not for friends"]}
+${F`hi friend!`}
+${N`not for friends`}
 1. a;
 2. b;
 bleh: heart:
 a
-${[0, "not for friends"]}
 `;
