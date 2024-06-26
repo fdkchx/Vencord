17e839cb40daf16b80984ecc77bb729262b895d2
Sqaaakoi <sqaaakoi-git@sqaaakoi.xyz>
2024-06-27 02:37:28 +1200

    undo something

--- src/plugins/_core/sqaaakoiForkSupport/WelcomeModal.tsx
+++ src/plugins/_core/sqaaakoiForkSupport/WelcomeModal.tsx
@@ -6,15 +6,14 @@
 
 import { DataStore } from "@api/index";
 import { closeModal, ModalContent, ModalFooter, ModalHeader, ModalProps, ModalRoot, ModalSize, openModal } from "@utils/modal";
-import { findByPropsLazy } from "@webpack";
-import { Button, Parser, RelationshipStore, Text, useEffect, useState } from "@webpack/common";
+import { extractAndLoadChunksLazy, findLazy } from "@webpack";
+import { Button, Flex, Parser, RelationshipStore, Text, useEffect, useState } from "@webpack/common";
 
 import { CURRENT_WELCOME_NOTICE_VERSION, SQAAAKOI_USER_ID, WELCOME_BACK_HEADER, WELCOME_HEADER, WELCOME_MESSAGE, WELCOME_NOTICE_VERSION_KEY } from "./constants";
 
-// const contentClass = findLazy(m => m.modal && m.content && Object.keys(m).length === 2);
+const contentClass = findLazy(m => m.modal && m.content && Object.keys(m).length === 2);
 // must be called before rendering
-// export const requireContentClass = extractAndLoadChunksLazy(["hasAnyModalOpen)())&&(0,"]);
-const ChatClasses = findByPropsLazy("chat", "content", "noChat", "chatContent");
+export const requireContentClass = extractAndLoadChunksLazy(["hasAnyModalOpen)())&&(0,"]);
 
 // First time run card
 export function WelcomeModal({ modalProps, close, isFriend, force, welcomeBack, text }: { modalProps: ModalProps; close: () => void; isFriend: boolean; force: boolean; welcomeBack: boolean; text: string; }) {
@@ -35,18 +34,20 @@ export function WelcomeModal({ modalProps, close, isFriend, force, welcomeBack, 
             <Text variant="heading-lg/semibold" style={{ flexGrow: 1, textAlign: "center" }}>{welcomeBack ? WELCOME_BACK_HEADER : WELCOME_HEADER}</Text>
         </ModalHeader>
         <ModalContent>
-            <div className={ChatClasses.chatContent}>
-                {Parser.parse(text, true, {})}
+            <div className={contentClass.content}>
+                {Parser.parse(text, false)}
             </div>
         </ModalContent>
         <ModalFooter>
-            <Button
-                color={Button.Colors.GREEN}
-                submitting={closing && !unlocked}
-                onClick={() => setClosing(true)}
-            >
-                Continue
-            </Button>
+            <Flex flexDirection="column">
+                <Button
+                    color={Button.Colors.GREEN}
+                    submitting={closing && !unlocked}
+                    onClick={() => setClosing(true)}
+                >
+                    Continue
+                </Button>
+            </Flex>
         </ModalFooter>
     </ModalRoot>;
 }
@@ -58,6 +59,7 @@ export async function openWelcomeModal(force: boolean) {
         currentVersion ??= 0;
         if (currentVersion >= CURRENT_WELCOME_NOTICE_VERSION) return;
     }
+    await requireContentClass();
     const isFriend = RelationshipStore.isFriend(SQAAAKOI_USER_ID);
     const key = openModal(modalProps => (
         <WelcomeModal
